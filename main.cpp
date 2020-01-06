ea703c139a268c33971413b01572cb7fb22f4938
ywqzzy <592838129@qq.com>
2020-01-06 10:21:32 +0800

    fix some input corner case

--- main.cpp
+++ main.cpp
@@ -22,8 +22,8 @@ typedef struct {
 
 typedef struct {
     uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 typedef struct {
@@ -43,7 +43,9 @@ typedef enum {
 
 typedef enum {
     PREPARE_SUCCESS,
+    PREPARE_NEGATIVE_ID,
     PREPARE_SYNTAX_ERROR,
+    PREPARE_STRING_TOO_LONG,
     PREPARE_UNRECOGNIZED_STATEMENT,
 }PrepareResult;
 
@@ -169,16 +171,40 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 }
 
 /*Statement*/
+/*
+If the string it’s reading is larger than the buffer it’s reading into, 
+it will cause a buffer overflow and start writing into unexpected places. 
+We want to check the length of each string before we copy it into a Row structure. 
+And to do that, we need to divide the input by spaces.
+*/
+
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
+    statement->type = STATEMENT_INSERT;
+
+    char* keyword   = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username  = strtok(NULL, " ");
+    char* email     = strtok(NULL, " ");
+    if(id_string == NULL || username == NULL || email == NULL) {
+        return PREPARE_SYNTAX_ERROR;
+    }
+    int id = atoi(id_string);
+    if (id < 0) return PREPARE_NEGATIVE_ID;
+    if (strlen(username) > COLUMN_USERNAME_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if(strlen(email) > COLUMN_EMAIL_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email,    email);
+    return PREPARE_SUCCESS;
+}
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(
-            input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
-            statement->row_to_insert.username, statement->row_to_insert.email);
-        if (args_assigned < 3) {
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
     }
     if (strcmp(input_buffer->buffer, "select") == 0) {
         statement->type = STATEMENT_SELECT;
@@ -229,7 +255,8 @@ int main(int argc, char* argv[]) {
 
         if(input_buffer->buffer[0] == '.') {
             switch(do_meta_command(input_buffer)) {
-                case(META_COMMAND_SUCCESS): continue;
+                case(META_COMMAND_SUCCESS): 
+                    continue;
                 case(META_COMMAND_UNRECOGNIZED_COMMAND):
                     printf("Unrecognized command '%s'\n", input_buffer->buffer);
                     continue;
@@ -239,6 +266,12 @@ int main(int argc, char* argv[]) {
         switch (prepare_statement(input_buffer, &statement)) {
             case (PREPARE_SUCCESS):
                 break;
+            case (PREPARE_NEGATIVE_ID):
+                printf("Id must be postive number.\n");
+                continue;
+            case (PREPARE_STRING_TOO_LONG):
+                printf("String is too long.\n");
+                continue;
             case (PREPARE_SYNTAX_ERROR):
                 printf("Syntax error. Could not parse statement.");
                 continue;
